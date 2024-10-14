bbfdf6a54a15da356562d3628590eb95f70fb12d
roeheatt <roeshahlr18@gmail.com>
2024-10-14 20:40:41 +0530

    dashboard layout updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),provideAnimations(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(),provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
